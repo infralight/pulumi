4f2072280b0bf1d568f3122ef7f8e1adfbb4912a
Gil Ben <71891726+1gilb@users.noreply.github.com>
2022-02-14 23:04:26 +0200

    Update lambda.go

--- refresher/utils/lambda.go
+++ refresher/utils/lambda.go
@@ -1,38 +1,13 @@
 package utils
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/hashicorp/go-multierror"
 	goKit "github.com/infralight/go-kit/aws"
 	"github.com/infralight/pulumi/refresher/config"
 	"github.com/rs/zerolog"
 )
-
-func TriggerFireflyEngine(cfg *config.Config) error {
-	sess := cfg.LoadAwsSession()
-	lambdaClient := lambda.New(sess)
-	payload, err := json.Marshal(map[string]interface{}{
-		"accountId":     cfg.AccountId,
-		"integrationId": cfg.ClientAWSIntegrationId,
-		"provider":      "aws",
-	})
-	if err != nil {
-		return fmt.Errorf("failed to marshal engine event payload: %v", err)
-	}
-	_, err = lambdaClient.Invoke(&lambda.InvokeInput{
-		FunctionName:   aws.String(cfg.FireflyEngineLambdaArn),
-		InvocationType: aws.String(lambda.InvocationTypeEvent),
-		Payload:        payload,
-	})
-	if err != nil {
-		return fmt.Errorf("falied to invoke engine lambda %s: %v", cfg.FireflyEngineLambdaArn, err)
-	}
-	return nil
-}
 
 func InvokeEngineLambda(cfg *config.Config, assetTypes []string, logger *zerolog.Logger) error {
 	var merr *multierror.Error
